8ac53751a8ee7f81ebc4b886c8009dc7acf59a6b
sanuga_synapse <sanuga@synapse.lk>
2024-11-17 10:32:35 +0530

    api-deployment test endpoint is created

--- index.mjs
+++ index.mjs
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import serverless from "serverless-http";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`core-service is up and running on port ${3000}`);
+dotenv.config();
+
+app.get("/tests", (request, response) => {
+  return response.send({ msg: "This is the api testing" });
 });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`core-service is up and running on port ${PORT}`);
+// });
+
+export const handler = serverless(app);
